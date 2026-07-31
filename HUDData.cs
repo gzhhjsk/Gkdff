4f351c6e63fd3cb5ae8094af9065917dcf9031e1
gzhhjsk <gnfhbn@atomicmail.io>
2026-08-01 00:39:42 +0330

    Add HUDData class for game HUD management

---
+++ HUDData.cs
@@ -0,0 +1,15 @@
+using System;
+
+[Serializable]
+public class HUDData
+{
+    public int health;
+    public int armor;
+
+    public int currentAmmo;
+    public int maxAmmo;
+
+    public string weaponName;
+
+    public bool isReloading;
+}
